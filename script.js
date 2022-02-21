7b551db2e6d297b5cabb5589f32352625fad5524
Mathieu JULIE <62996766+Dipsy974@users.noreply.github.com>
2022-02-22 00:49:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+notes = document.querySelectorAll(".note");
+codeDOM = document.getElementById("codecache"); 
+
+song = ["CD","E","F","CD","E","F","CD","E","F","D"]; 
+step = 0;
+sequence_initialized = false; 
+
+
+notes.forEach(note => {
+    note.addEventListener('click', () => playNote(note));
+});
+
+
+function playNote(note){
+    const noteSound = document.getElementById(note.dataset.note);
+    noteSound.currentTime = 0; 
+    noteSound.play(); 
+    note.classList.add("active"); 
+    noteSound.addEventListener("ended", () => {
+        note.classList.remove("active"); 
+    });
+
+    if(sequence_initialized){
+        if(note.dataset.note == song[step]){
+            step++;
+        }else{
+            step = 0;
+            sequence_initialized = false; 
+        }
+    }
+
+    if(step == 0 && note.dataset.note == "CD"){
+        console.log("ui"); 
+        sequence_initialized = true;
+        step ++; 
+    }
+    
+    if(step > 9){
+        unfade(codeDOM);  
+    }
+    
+}
+
+function unfade(element) {
+    var opacity = 0.01;  // initial opacity
+    element.classList.add("active"); 
+    var timer = setInterval(function () {
+        if (opacity >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = opacity;
+        element.style.filter = 'alpha(opacity=' + opacity * 100 + ")";
+        opacity += opacity * 0.1;
+    }, 50);
+}
